2d211b09fc034d7597452295a366739694338482
Kevin Long <kevin.uu.long@gmail.com>
2021-03-14 00:11:48 -0500

    Basic UI and some functionality
    Still VERY buggy!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { app, BrowserWindow } = require('electron');
-const {Howler, Howl} = require('howler');
+const { Howler, Howl } = require('howler');
 
+require('electron-reload')(__dirname);
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+	app.quit();
 }
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -11,28 +12,31 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 let mainWindow;
 
 const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    },
-  });
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 675,
+		height: 275,
+		maximizable: false,
+		webPreferences: {
+			nodeIntegration: true
+		},
+		frame: false,
+		transparent: true
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+	// and load the index.html of the app.
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+	// Emitted when the window is closed.
+	mainWindow.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 };
 
 // This method will be called when Electron has finished
@@ -42,19 +46,19 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
--- src/main.js
+++ src/main.js
@@ -1,10 +1,117 @@
-window.onload = function(){
+window.onload = () => {
 
-  const {Howler, Howl} = require('howler');
-  
-  var sound = new Howl({
-      src: ['sound.mp3']
-  });
-    
-  sound.play();
+	const { Howler, Howl } = require('howler');
+	const mm = require('music-metadata');
+	const { remote } = require('electron');
+	let browserWindow = remote.getCurrentWindow();
+
+	// Get ready to play
+	let sound = new Howl({
+		src: ['C:\\Users\\Kevin Long\\Documents\\GitHub\\audioplayer\\src\\music\\Olivia Rodrigo - drivers license.mp3'],
+		volume: 1.5
+	});
+
+	// Get and load the basic metadata
+	(async () => {
+		try {
+			const { common } = await mm.parseFile('C:\\Users\\Kevin Long\\Documents\\GitHub\\audioplayer\\src\\music\\Olivia Rodrigo - drivers license.mp3');
+			document.querySelector("#trackName").textContent = common.title;
+			document.querySelector("#artistName").textContent = common.artists.toString().replace(/(.),(.)/gm, '$1, $2');
+			document.querySelector("#albumArt").src = `data:${common.picture[0].format};base64,${common.picture[0].data.toString('base64')}`;
+		} catch (error) {
+			console.error(error.message);
+		}
+	})();
+
+	// Play
+	document.querySelector("#play").addEventListener('click', () => {
+		sound.play();
+		updateProgress();
+	})
+
+	sound.on('play', () => {
+		document.querySelector("#play").style.display = "none";
+		document.querySelector("#pause").style.display = "inline-block";
+	})
+
+	// Pause
+	document.querySelector("#pause").addEventListener('click', () => {
+		sound.pause();
+	})
+
+	sound.on('pause', () => {
+		document.querySelector("#pause").style.display = "none";
+		document.querySelector("#play").style.display = "inline-block";
+	})
+
+	// Previous
+	document.querySelector("#prev").addEventListener('click', () => {
+		if (sound.seek() >= 2) {
+			sound.seek(0);
+		}
+	})
+
+	// Next
+	document.querySelector("#next").addEventListener('click', () => {
+		sound.seek(sound.duration());
+	})
+
+	sound.on('seek', () => {
+		updateProgress(true);
+	})
+
+	function updateProgress() {
+		seek = false;
+		if (dragging || sound.playing() || seek) {
+			// console.log(dragging, seek, sound.playing());
+			let percent = ((sound.seek() / sound.duration()) * 100);
+			document.querySelector('#slider-bar').style.transform = `translateX(-${100 - percent}%)`;
+			document.querySelector('#slider-thumb').style.left = `${percent - 1}%`;
+			window.requestAnimationFrame(updateProgress);
+		}
+	}
+
+	let dragging = false;
+	function dragThumb(e) {
+		if (
+			dragging
+			&& e.pageX >= document.querySelector('#slider-wrapper').getBoundingClientRect().left
+			&& e.pageX <= document.querySelector('#slider-wrapper').getBoundingClientRect().right
+		) {
+			// document.querySelector('#slider-thumb').style.left = `${}px`;
+			// console.log(e.pageX, document.querySelector('#slider-wrapper').getBoundingClientRect().right)
+			let percent = (e.pageX - 25) / document.querySelector('#slider-wrapper').getBoundingClientRect().right;
+			// console.log(percent, sound.duration(), percent * sound.duration());
+			sound.seek(percent * sound.duration());
+			window.requestAnimationFrame(dragThumb)
+		}
+	}
+
+	document.querySelector("#slider-thumb").addEventListener('mousedown', (e) => {
+		dragging = true;
+		// updateProgress();
+		dragThumb(e);
+	})
+
+	document.addEventListener('mouseup', () => {
+		if (dragging) {
+			dragging = false;
+			
+		}
+	})
+
+	document.addEventListener('mousemove', (e) => {
+		if (dragging) { dragThumb(e); }
+	})
+
+	// Minimize
+	document.querySelector("#minimize").addEventListener('click', () => {
+		browserWindow.minimize();
+	})
+
+	// Close
+	document.querySelector("#close").addEventListener('click', () => {
+		browserWindow.close();
+	})
+
 }
